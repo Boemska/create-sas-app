74c2321f371c53ec6d4bc5aef3627dadacd7f4ca
milosveljkovic <veljkovicmilos559@gmail.com>
2020-06-29 11:31:05 +0200

    Sharing project with QRCode

---
+++ onsen-ui/build/precache-manifest.ddae8577c1ce278fbec1c9442e264b65.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "063701b35e9784442b86ba3f69429d7c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6f482dc4372b3ed6ce03",
+    "url": "./static/css/2.1caee176.chunk.css"
+  },
+  {
+    "revision": "5502d56540de4e015c46",
+    "url": "./static/css/main.69fd8171.chunk.css"
+  },
+  {
+    "revision": "6f482dc4372b3ed6ce03",
+    "url": "./static/js/2.1abecc15.chunk.js"
+  },
+  {
+    "revision": "a72b1a4bdd9dd43040b5d2ad62ab065f",
+    "url": "./static/js/2.1abecc15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5502d56540de4e015c46",
+    "url": "./static/js/main.cc9c8d83.chunk.js"
+  },
+  {
+    "revision": "c9d5dea798fcefa32686",
+    "url": "./static/js/runtime-main.43b3872e.js"
+  },
+  {
+    "revision": "a4d31128b633bc0b1cc1f18a34fb3851",
+    "url": "./static/media/Material-Design-Iconic-Font.a4d31128.woff2"
+  },
+  {
+    "revision": "b351bd62abcd96e924d9f44a3da169a7",
+    "url": "./static/media/Material-Design-Iconic-Font.b351bd62.ttf"
+  },
+  {
+    "revision": "d2a55d331bdd1a7ea97a8a1fbb3c569c",
+    "url": "./static/media/Material-Design-Iconic-Font.d2a55d33.woff"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "./static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "2ef8ba3410dcc71578a880e7064acd7a",
+    "url": "./static/media/fa-brands-400.2ef8ba34.woff"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "./static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "5bf145531213545e03ff41cd27df7d2b",
+    "url": "./static/media/fa-brands-400.5bf14553.svg"
+  },
+  {
+    "revision": "5e2f92123d241cabecf0b289b9b08d4a",
+    "url": "./static/media/fa-brands-400.5e2f9212.woff2"
+  },
+  {
+    "revision": "98b6db59be947f563350d2284fc9ea36",
+    "url": "./static/media/fa-brands-400.98b6db59.ttf"
+  },
+  {
+    "revision": "a7b95dbdd87e0c809570affaf366a434",
+    "url": "./static/media/fa-brands-400.a7b95dbd.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "./static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "./static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "./static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "427d721b86fc9c68b2e85ad42b69238c",
+    "url": "./static/media/fa-regular-400.427d721b.woff"
+  },
+  {
+    "revision": "5eb754ab7dbd2fee562360528db4c3c0",
+    "url": "./static/media/fa-regular-400.5eb754ab.svg"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "./static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "65b9977aa23185e8964b36eddbce7a20",
+    "url": "./static/media/fa-regular-400.65b9977a.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "./static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "./static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "./static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "dcce4b7fbd5e895561e18af4668265af",
+    "url": "./static/media/fa-regular-400.dcce4b7f.eot"
+  },
+  {
+    "revision": "e6257a726a0cf6ec8c6fec22821c055f",
+    "url": "./static/media/fa-regular-400.e6257a72.woff2"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "./static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "./static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "./static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "418dad87601f9c8abd0e5798c0dc1feb",
+    "url": "./static/media/fa-solid-900.418dad87.woff2"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "./static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "46e7cec623d8bd790d9fdbc8de2d3ee7",
+    "url": "./static/media/fa-solid-900.46e7cec6.eot"
+  },
+  {
+    "revision": "49279363201ed19a840796e05a74a91b",
+    "url": "./static/media/fa-solid-900.49279363.svg"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "./static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "a7140145ebaaf5fb14e40430af5d25c4",
+    "url": "./static/media/fa-solid-900.a7140145.woff"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "./static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "ff8d9f8adb0d09f11d4816a152672f53",
+    "url": "./static/media/fa-solid-900.ff8d9f8a.ttf"
+  },
+  {
+    "revision": "08eda92397aee9e52a40d431c3a28e5c",
+    "url": "./static/media/glyphicons-halflings-regular.08eda923.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "./static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "./static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "./static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "./static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "665921072642ed354618b32af7425a22",
+    "url": "./static/media/ionicons.66592107.svg"
+  },
+  {
+    "revision": "7f9fdd5e7c0656fea97141765f3e0b50",
+    "url": "./static/media/ionicons.7f9fdd5e.eot"
+  },
+  {
+    "revision": "96f1c901c087fb64019f7665f7f8aca6",
+    "url": "./static/media/ionicons.96f1c901.woff2"
+  },
+  {
+    "revision": "c37ad37a3a23417b739ac3b297416201",
+    "url": "./static/media/ionicons.c37ad37a.woff"
+  },
+  {
+    "revision": "cfdc15225683b7529d6ba1e9d8a9be59",
+    "url": "./static/media/ionicons.cfdc1522.ttf"
+  }
+]);
