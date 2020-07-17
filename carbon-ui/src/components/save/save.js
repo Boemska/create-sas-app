db1e3d2ee79e47bcd73a2c7778512bfaea7cf2be
igor <igor@boemskats.com>
2020-07-17 15:31:40 +0200

    Improvemenets to save feature

--- carbon-ui/src/components/save/save.js
+++ carbon-ui/src/components/save/save.js
@@ -43,7 +43,7 @@ const Save = (props) => {
 		const dataObj = {
 			file: [blob, fileName]
 		}
-		const res = updateFile(dispatch, projectMetadata.uri, dataObj, new Date(projectMetadata.modifiedTimeStamp));
+		const res = updateFile(dispatch, projectMetadata.uri, blob, projectContent.lastModified);
 		res.then(result => {
 			dispatch({
 				type: ActionTypes.CHANGES_SAVED,
