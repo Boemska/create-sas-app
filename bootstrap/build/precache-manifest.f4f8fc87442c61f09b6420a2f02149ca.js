c5d6c68abac9728f17f19ec76942adbb71c660c4
Bozidar Zecevic <bozazec@gmail.com>
2021-01-25 23:33:04 +0100

    Build app

---
+++ bootstrap/build/precache-manifest.f4f8fc87442c61f09b6420a2f02149ca.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b60d29ed8ee4e54b7e9873548dbb0e3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "71b04ea7e14cf0c4a140",
+    "url": "./static/css/2.606061db.chunk.css"
+  },
+  {
+    "revision": "d7b2e7aa99bd507090bd",
+    "url": "./static/css/main.3cce0d5d.chunk.css"
+  },
+  {
+    "revision": "71b04ea7e14cf0c4a140",
+    "url": "./static/js/2.f4cc2ac2.chunk.js"
+  },
+  {
+    "revision": "d6fe92449a5bf7b5e05ebc1e57abd5a7",
+    "url": "./static/js/2.f4cc2ac2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7b2e7aa99bd507090bd",
+    "url": "./static/js/main.1c83c80e.chunk.js"
+  },
+  {
+    "revision": "c9d5dea798fcefa32686",
+    "url": "./static/js/runtime-main.43b3872e.js"
+  },
+  {
+    "revision": "2e91e50cfa66ca263a8273a48dda26de",
+    "url": "./static/media/Metropolis-Regular.2e91e50c.otf"
+  },
+  {
+    "revision": "3440b60515f3e9b38279956ce0bd3e52",
+    "url": "./static/media/Metropolis-Regular.3440b605.ttf"
+  },
+  {
+    "revision": "487ca609d2ef0d3448d024e7e672bf4b",
+    "url": "./static/media/Metropolis-Regular.487ca609.eot"
+  },
+  {
+    "revision": "4a405762789758475f736592772ebfd1",
+    "url": "./static/media/Metropolis-Regular.4a405762.woff2"
+  },
+  {
+    "revision": "663b4fcc5530ccf49d6b31a9450b02a0",
+    "url": "./static/media/Metropolis-Regular.663b4fcc.woff"
+  },
+  {
+    "revision": "e6834d2801c106d4d0ec30a5430ccd37",
+    "url": "./static/media/eu-transparent.e6834d28.png"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "./static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "./static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "./static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "./static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "./static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "./static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "./static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "./static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "./static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "./static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "./static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "./static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "./static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "./static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "./static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "08eda92397aee9e52a40d431c3a28e5c",
+    "url": "./static/media/glyphicons-halflings-regular.08eda923.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "./static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "./static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "./static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "./static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "147e3378b44bc9570418b1eece10dd7c",
+    "url": "./static/media/materialdesignicons-webfont.147e3378.woff"
+  },
+  {
+    "revision": "174c02fc4609e8fc4389f5d21f16a296",
+    "url": "./static/media/materialdesignicons-webfont.174c02fc.ttf"
+  },
+  {
+    "revision": "64d4cf64afd77a4ad2713f648eb920e6",
+    "url": "./static/media/materialdesignicons-webfont.64d4cf64.eot"
+  },
+  {
+    "revision": "7a44ea195f395e1d086010e44555a5c4",
+    "url": "./static/media/materialdesignicons-webfont.7a44ea19.woff2"
+  }
+]);
